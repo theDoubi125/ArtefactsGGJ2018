5efd83fcd89352f011fa8efac90fba8b234e8f7d
theDoubi125 <julien.revel@telecomnancy.net>
2018-01-27 15:45:04 +0100

    Ajout fichiers menu

---
+++ Global Game Jam 2018/Assets/Scripts/GameLauncher.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameLauncher : MonoBehaviour {
+    public string gameSceneName;
+
+    public void LaunchGame()
+    {
+        SceneManager.LoadScene(gameSceneName, LoadSceneMode.Single);
+    }
+}
