eea7c0b420ee0466df503dd9aade67594b5706d7
theDoubi125 <julien.revel@telecomnancy.net>
2018-01-27 14:56:07 +0100

    Gestion de la manette

--- Global Game Jam 2018/Assets/Scripts/PlayerController.cs
+++ Global Game Jam 2018/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float acceleration = 10;
     public int playerIndex = 1;
     public Vector2 direction = Vector2.right;
+    public float directionMinLength = 0.5f;
 
     private string playerInputPrefix;
     private Rigidbody body;
@@ -19,13 +20,13 @@ public class PlayerController : MonoBehaviour
 	
 	void Update ()
     {
-        body.AddForce(acceleration * new Vector3(Input.GetAxis(playerInputPrefix + "Move_X"), 0, Input.GetAxis(playerInputPrefix + "Move_Y")));
+        body.AddForce(acceleration * (new Vector3(Input.GetAxis(playerInputPrefix + "Move_X"), 0, Input.GetAxis(playerInputPrefix + "Move_Y"))).normalized);
         Vector3 direction = new Vector3(Input.GetAxis(playerInputPrefix + "Look_X"), 0, Input.GetAxis(playerInputPrefix + "Look_Y"));
         float angle = Vector3.Angle(Vector2.right, direction);
         if (direction.z < 0)
             angle *= -1;
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.up);
-        if(direction.sqrMagnitude > 0)
+        if(direction.sqrMagnitude > directionMinLength * directionMinLength)
             transform.SetPositionAndRotation(transform.position, rotation);
 	}
 
