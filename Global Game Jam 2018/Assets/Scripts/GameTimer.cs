f4745f7e4d7fc1dbeeb3c1003afeae9ab741c724
theDoubi125 <julien.revel@telecomnancy.net>
2018-01-28 08:16:45 +0100

    Ajout timer wip

---
+++ Global Game Jam 2018/Assets/Scripts/GameTimer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTimer : MonoBehaviour
+{
+    public float MatchDuration;
+    public Transform victoryMatchResultMenu;
+    public Transform defeatMatchResultMenu;
+    private float MatchTime;
+
+    void Start()
+    {
+        defeatMatchResultMenu.gameObject.SetActive(false);
+    }
+
+    public float GetRemainingTime()
+    {
+        return MatchDuration - MatchTime;
+    }
+
+    void Update()
+    {
+        MatchTime += Time.deltaTime;
+        if (MatchTime >= MatchDuration)
+        {
+            GameController.instance.PauseGame();
+            defeatMatchResultMenu.gameObject.SetActive(true);
+        }
+    }
+}
