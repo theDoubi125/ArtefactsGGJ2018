087c0d35f18c104da181f3ceb65ff1130b44d3ae
theDoubi125 <julien.revel@telecomnancy.net>
2018-01-28 10:10:25 +0100

    Gestion fin de mission et retour au character select screen

--- Global Game Jam 2018/Assets/Scripts/GameOverScreen.cs
+++ Global Game Jam 2018/Assets/Scripts/GameOverScreen.cs
@@ -1,22 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOverScreen : MonoBehaviour
 {
     Animation anim;
+    Animator animator;
 
 	void Start ()
     {
+        animator = GetComponent<Animator>();
 	}
+
+    void Update()
+    {
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        bool keyDown = Input.anyKeyDown;
+        if (keyDown && stateInfo.IsTag("End"))
+        {
+            GameController.instance.controllers.Clear();
+            GameController.instance.isGamePaused = false;
+            SceneManager.LoadScene("Character select screen");
+        }
+    }
 	
     public void Show(int player)
     {
-        GetComponent<Animator>().SetTrigger("Show Menu");
+        animator.SetTrigger("Show Menu");
 	}
 
     public void ShowFailure()
     {
-        GetComponent<Animator>().SetTrigger("Show Menu");
+        animator.SetTrigger("Show Menu");
     }
 }
