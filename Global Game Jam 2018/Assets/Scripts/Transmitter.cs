88b0515304c41e517d9cf75f555d445bd0431f15
Eric Wilain <ericwilain@gmail.com>
2018-01-28 04:00:00 +0100

    MaJ Transmitter

---
+++ Global Game Jam 2018/Assets/Scripts/Transmitter.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transmitter : MonoBehaviour
+{
+	public float maxValue = 100f;
+	public float startValue = 0f;
+	public float transmissionSpeed = 1f;
+	public float bonusFactor = 1f;
+	public bool[] arePlayersChanneling = { false, false, false, false };
+	public Vector3[] gaugesInitialPositions;
+	public Vector3[] gaugesFinalPositions;
+	public float[] playerScores = {0f,0f,0f,0f};
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		for (int i = 0; i < 4; i++)
+		{
+			if (arePlayersChanneling [i]) {
+				playerScores [i] += Time.deltaTime * transmissionSpeed * bonusFactor;
+
+			}
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		ActionController tmp = col.GetComponent<ActionController> ();
+		if (tmp != null)
+		{
+			//TODO : Ajouter variables dans ActionController
+			//tmp.isInRangeOfTransmitter = true;
+		}
+	}
+
+	void OnTriggerExit(Collider col)
+	{
+		ActionController tmp = col.GetComponent<ActionController> ();
+		if (tmp != null)
+		{
+			//TODO : Ajouter variables dans ActionController
+			//tmp.isInRangeOfTransmitter = false;
+		}
+	}
+}
