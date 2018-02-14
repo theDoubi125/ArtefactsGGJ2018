419fedd9ad4cfb2f042a1403ad474ad534a7c7fc
Doubi125 <doubi125@hotmail.fr>
2018-02-14 18:25:47 +0100

    Ajout d'une arme avec un tir chargé

---
+++ Global Game Jam 2018/Assets/Scripts/Combat/Weapons/ChargeWeapon.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChargeWeapon : MonoBehaviour
+{
+	public WeaponAction actionToBind;
+	public bool throwOnShoot = false;
+
+	public Transform projectilePrefab;
+	public float precision = 10;
+	private Weapon weapon;
+	private Magazine magazine;
+	private PlayerController player;
+	private Inventory inventory;
+	public float throwDistance = 2;
+	public float projectilePerShot = 1;
+
+	public float maxCharge = 2;
+	private bool isCharging = false;
+	private float charge = 0;
+
+
+	void OnEnable()
+	{
+		player = GetComponentInParent<PlayerController>();
+		inventory = GetComponentInParent<Inventory>();
+		weapon = GetComponent<Weapon>();
+		weapon.BindToAction(actionToBind, AttackPressed, AttackReleased);
+		weapon.AttackReleased += AttackReleased;
+		magazine = GetComponent<Magazine>();
+	}
+
+	void OnDisable()
+	{
+		weapon.UnbindAction(actionToBind, AttackPressed, AttackReleased);
+	}
+
+	void AttackPressed()
+	{
+		isCharging = true;
+		charge = 0;
+	}
+
+	void AttackReleased()
+	{
+		if (isCharging)
+		{
+			List<Transform> projectiles = Shoot();
+			foreach(Transform projectile in projectiles)
+			{
+				ChargeProjectile chargeProjectile = projectile.GetComponent<ChargeProjectile> ();
+				chargeProjectile.charge = charge;
+			}
+			isCharging = false;
+		}
+	}
+
+	void Update()
+	{
+		if (isCharging)
+			charge += Time.deltaTime;
+	}
+
+	private List<Transform> Shoot()
+	{
+		List<Transform> result = new List<Transform>();
+		if (magazine.HasAmmo())
+		{
+			for (int i = 0; i < projectilePerShot; i++)
+			{
+				Transform projectileTransform = Instantiate<Transform> (projectilePrefab);
+				result.Add (projectileTransform);
+				Vector3 angles = new Vector3 (0, Gaussian () * precision / 2f, 0);
+				Vector3 shootDirection = Quaternion.Euler (angles) * player.GetWeaponDirection ().normalized;
+				projectileTransform.position = transform.position + shootDirection * throwDistance;
+				projectileTransform.rotation = Quaternion.LookRotation (shootDirection);
+				Inventory projectileInventory = projectileTransform.GetComponentInChildren<Inventory> ();
+				if (i == 0 && throwOnShoot && projectileInventory != null) {
+					projectileInventory.AddWeapon (weapon);
+					inventory.UpdateWeapons ();
+				}
+			}
+			magazine.UseAmmo();
+		}
+		return result;
+	}
+
+	public static float Gaussian()
+	{
+		float u, v, S;
+
+		do
+		{
+			u = 2.0f * Random.value - 1.0f;
+			v = 2.0f * Random.value - 1.0f;
+			S = u * u + v * v;
+		}
+		while (S >= 1.0f);
+
+		float fac = Mathf.Sqrt(-2.0f * Mathf.Log(S) / S);
+		return u * fac;
+	}
+}
